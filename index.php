ad74c467a0913ad7169482eafce7e7df75a538ad
Bez <bezlash@gmail.com>
2014-06-25 21:46:35 +0000

    Initial commit for my journey to learning php and mysql. Containing an index.php file and a mysql_test.php file to test whether php can connect to mysql

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+	<title>Learn PHP and MySQL</title>
+</head>
+
+<body>
+	<p>This is a html line</p>
+	<?php 
+		echo "<p>This is a PHP line</p>"; 
+		phpinfo(); 
+	?>
+</body>
+
+</html>
